5cd11ade746c5d549b956bc981a9bfdb4fff73ef
Andres Beltran <anjubelquin@gmail.com>
2025-09-24 19:45:49 -0500

    added more bottoms, ot looks bbetter, and lets you add to your list

--- PracticeApp/app/index.tsx
+++ PracticeApp/app/index.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState<string[]>([]);
+
+  function goalInputHandler(enteredText: string) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
+  }
+    
+
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' />
-        <Button title='Add Goal' />
+        <TextInput style={styles.textInput} placeholder="Your course goal!" onChangeText={goalInputHandler} />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals</Text>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
       </View>
     </View>
 
@@ -17,11 +30,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
+
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
   },
   textInput: {
     borderWidth: 1,
@@ -29,5 +49,9 @@ const styles = StyleSheet.create({
     width: '80%',
     marginRight: 8,
     padding: 8
+  },
+  goalsContainer: {
+    flex: 5,
+    padding: 24
   }
 });
